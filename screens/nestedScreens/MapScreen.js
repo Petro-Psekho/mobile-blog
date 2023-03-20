656a7a606cb6e43f0137a91ac40bb2ddd11cb8de
Petro Psekho <psekho.pp@gmail.com>
2023-03-20 16:10:01 +0200

    add map

--- screens/nestedScreens/MapScreen.js
+++ screens/nestedScreens/MapScreen.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 const MapScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>MapScreen</Text>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 50.44961659850182,
+          longitude: 30.525125578548096,
+          latitudeDelta: 0.00522,
+          longitudeDelta: 0.00221,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: 50.44961659850182,
+            longitude: 30.525125578548096,
+          }}
+          title="my photo"
+        />
+      </MapView>
     </View>
   );
 };
@@ -12,8 +29,6 @@ const MapScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
 
