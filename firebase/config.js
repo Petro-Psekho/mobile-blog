4e8f7b71377101a4f924d5b00ebc999e360abd64
Petro Psekho <psekho.pp@gmail.com>
2023-03-21 16:14:04 +0200

    add authSignUpUser

--- firebase/config.js
+++ firebase/config.js
@@ -1,24 +1,28 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import 'firebase/auth';
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCHXuhyfcRhKnVvP26e4OuGxhgl04smcvw',
-  authDomain: 'mobile-blog-ce8b1.firebaseapp.com',
-  projectId: 'mobile-blog-ce8b1',
-  storageBucket: 'mobile-blog-ce8b1.appspot.com',
-  messagingSenderId: '514832336247',
-  appId: '1:514832336247:web:5a6e722f1b0dd407ba8d72',
-  measurementId: 'G-S91JQXNDJ3',
+  apiKey: "AIzaSyCHXuhyfcRhKnVvP26e4OuGxhgl04smcvw",
+  authDomain: "mobile-blog-ce8b1.firebaseapp.com",
+  projectId: "mobile-blog-ce8b1",
+  storageBucket: "mobile-blog-ce8b1.appspot.com",
+  messagingSenderId: "514832336247",
+  appId: "1:514832336247:web:5a6e722f1b0dd407ba8d72",
+  measurementId: "G-S91JQXNDJ3",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+// const analytics = getAnalytics(app);
 
-export default app;
+// export default app;
--- redux/auth/authOperation.js
+++ redux/auth/authOperation.js
@@ -1,3 +1,26 @@
-const authSignInUser = () => async (dispatch, getState) => {};
-const authSignUpUser = () => async (dispatch, getState) => {};
-const authSignOutUser = () => async (dispatch, getState) => {};
+import { db, auth } from "../../firebase/config";
+console.log("db------>", db);
+console.log("auth------>", auth);
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+export const authSignUpUser =
+  ({ email, password, login }) =>
+  async (dispatch, getState) => {
+    console.log("email, password, login ----->", email, password, login);
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+  };
+export const authSignInUser = () => async (dispatch, getState) => {};
+export const authSignOutUser = () => async (dispatch, getState) => {};
